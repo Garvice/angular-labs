85a8e7411d89de29bee15897c6e4dfcad5b9c016
Garvice Eakins <garvice.eakins@smartsheet.com>
2017-03-12 07:02:50 -0700

    updated Joke model

--- src/app/models/joke.model.ts
+++ src/app/models/joke.model.ts
@@ -1,12 +1,5 @@
 import {Vote} from "./vote.model";
 
-const CLASSIC_CHICKEN_JOKE = {
-    "setup": "Why did the chicken cross the road?",
-    "punchline": "To get to the other side",
-    "groanVotes": {"votes": 9},
-    "lolsVotes": {"votes": 3}
-};
-
 /**
  * Provides the ability to create a JOKE with a setup and punchline.
  * Tracks the total lols and groans the JOKE received over time.
@@ -15,7 +8,7 @@ export class Joke {
     private groanVotes: Vote = new Vote(0);
     private lolVotes: Vote = new Vote(0);
 
-    public static getJoke: any = CLASSIC_CHICKEN_JOKE;
+    public static getJoke: Joke = new Joke("Why did the chicken cross the road?", "To get to the other side", 3, 9);
 
     constructor(public setup: string, public punchline: string, lols?: number, groans?: number) {
         if (lols) {
