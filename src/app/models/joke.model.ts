00ba958e1f37ab8c1207c3a6afe10d1ac9f64842
Garvice Eakins <garvice.eakins@smartsheet.com>
2017-03-12 06:45:32 -0700

    Renamed joke functions

--- src/app/models/joke.model.ts
+++ src/app/models/joke.model.ts
@@ -17,7 +17,7 @@ export class Joke {
 
     public static getJoke: any = CLASSIC_CHICKEN_JOKE;
 
-    constructor(public setup: string, public punchline: string, lols?: number, groans?: number){
+    constructor(public setup: string, public punchline: string, lols?: number, groans?: number) {
         if (lols) {
             this.lolsVotes = new Vote(lols);
         }
@@ -39,7 +39,44 @@ export class Joke {
         return this.lolsVotes.voteCount();
     }
 
-    addLol(): void{
+    addLol(): void {
         this.lolsVotes.increment();
     }
 }
+
+/**
+ * Provides the ability to create a JOKE with a setup and punchline.
+ * Tracks the total lols and groans the JOKE received over time.
+ */
+export class Joke {
+    private groanVotes: Vote = new Vote(0);
+    private lolVotes: Vote = new Vote(0);
+
+    constructor(public id: number, public setup: string, public punchline: string, public categories: Array<string>,
+                private lols?: number, private groans?: number) {
+        if (lols) {
+            this.lolVotes = new Vote(lols);
+        }
+
+        if (groans) {
+            this.groanVotes = new Vote(groans);
+        }
+    }
+
+    groanCount(): number {
+        return this.groanVotes.voteCount();
+    }
+
+    addGroan(): void {
+        this.groanVotes.increment();
+    }
+
+    lolCount(): number {
+        return this.lolVotes.voteCount();
+    }
+
+    addLol(): void {
+        this
+    }
+}
+
